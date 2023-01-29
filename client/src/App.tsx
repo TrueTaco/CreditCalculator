fa392bebdafa34ede6f6805b5e55b714ad9720d3
Michel Depcik <inf19101@lehre.dhbw-stuttgart.de>
2023-01-29 18:40:54 +0100

    Add audiences

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Main from "./components/Main";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <Main />
     </div>
   );
 }
